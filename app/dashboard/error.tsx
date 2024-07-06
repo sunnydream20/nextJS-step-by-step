477bb1d0bdd2b981f9e05af2c959b5a36d73d929
unknown <timonpoo220@gmail.com>
2024-07-06 13:05:50 -0700

    feat: route groups

---
+++ app/dashboard/error.tsx
@@ -0,0 +1,18 @@
+'use client' // Error components must be Client Components
+ 
+import { useEffect } from 'react'
+ 
+export default function Error({
+    error,
+    reset,
+  }: {
+    error: Error & { digest?: string }
+    reset: () => void
+  }) {
+    return (
+      <div>
+        <h2>Something went wrong!</h2>
+        <button onClick={() => reset()}>Try again</button>
+      </div>
+    )
+  }
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,17 @@
-export default function Page() {
-    return <h1>Hello, Dashboard Page!</h1>
-  }
+import { Suspense } from 'react'
+// import { PostFeed, Weather } from './Components'
+import Loading from "./loading";
+ 
+export default function Posts() {
+  return (
+    <section>
+      <Suspense fallback={<p>Loading feed...</p>}>
+        <Loading />
+      </Suspense>
+      <br />
+      <Suspense fallback={<p>Loading weather...</p>}>
+        <Loading />
+      </Suspense>
+    </section>
+  )
+}
